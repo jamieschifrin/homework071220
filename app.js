51eaa85da7d488521fd41f5a342700415b0d8e0b
Jamie Schrifrin <jamieschifrin@hotmail.com>
2020-07-13 09:54:12 -0400

    ist

---
+++ app.js
@@ -0,0 +1,61 @@
+var test = 78;
+var cars = 79;
+​
+function add(cats, dogs) {
+    return cats + dogs ;
+}
+​
+console.log(add(test,cars));
+​
+//
+​
+function sub(boy3, girl4) {
+    return boy3 - girl4 ;
+}
+​
+console.log(sub(test,cars))
+​
+//
+​
+function mult(boy5, girl6) {
+    return boy5 * girl6 ;
+}
+​
+console.log(mult(test,cars))
+​
+//
+​
+function divi(boy7, girl8) {
+    return boy7 / girl8 ;
+}
+​
+console.log(divi(test,cars))
+​
+// Arrow Functions
+​
+var bricks = (Y, Z) => {return Y + Z}
+    console.log(bricks(50,1))
+​
+//
+​
+var sheep = (W, X) => {return W - X}
+console.log(sheep(99,1))
+​
+//
+​
+var cows = (R, S) => {return S * R}
+console.log(cows(10,10))
+​
+//
+​
+var friends = (T, V) => {return T / V}
+console.log(friends(6,3))
+Collapse
+
+
+
+
+
+
+
+
